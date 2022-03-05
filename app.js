687b1f43baf90b2f6034a7910b7554a6989c86f0
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 09:32:18 +0800

    嘗試在雲端使用socket.io 3

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ const router = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// socket設定
-const server = require('http').createServer(app)
-const buildSocket = require('./server')
-buildSocket(server)
-server.listen(PORT)
+// socket設定(原本)
+// const server = require('http').createServer(app)
+// const buildSocket = require('./server')
+// buildSocket(server)
+// server.listen(PORT)
+const socketIO = require('socket.io')
+const INDEX = '/index.html'
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+const io = socketIO(server)
+io.on('connection', (socket) => {
+  console.log('Client connected')
+  socket.on('disconnect', () => console.log('Client disconnected'))
+})
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
 
 app.use(passport.initialize())
 app.use(express.urlencoded({ extended: true }))
