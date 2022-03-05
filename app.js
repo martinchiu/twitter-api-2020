fcb33ad9b9951b17a6df2041c84b64e319a420c2
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 10:16:44 +0800

    嘗試在雲端使用socket.io 5

--- app.js
+++ app.js
@@ -19,7 +19,24 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 // socket設定
-const buildSocket = require('./server')
-buildSocket(app)
+const socketIO = require('socket.io')
+const meaasge = require('./server/modules/message')
+const io = socketIO(app, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'],
+    credentials: true
+  },
+  rejectUnauthorized: false
+})
+io.on('connection', (socket) => {
+  // 可以在伺服器端顯示通道過來的所有事件，以及相關的參數
+  socket.onAny((event, ...args) => {
+    console.log(event, args)
+  })
+  socket.on('disconnect', (reason) => console.log(reason))
+  meaasge(io, socket)
+})
 
 module.exports = app
