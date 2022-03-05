931ca5e6d925c4c511c316d561f9a3fc7ac79c82
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 10:40:24 +0800

    嘗試在雲端使用socket.io 7

--- app.js
+++ app.js
@@ -12,29 +12,13 @@ const PORT = process.env.PORT || 3000
 
 // socket設定(原本)
 // const server = require('http').createServer(app)
-// const buildSocket = require('./server')
 // buildSocket(server)
 // server.listen(PORT)
-const socketIO = require('socket.io')
-const INDEX = '/index.html'
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-const io = socketIO(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'],
-    credentials: true
-  },
-  rejectUnauthorized: false
-})
-io.on('connection', (socket) => {
-  console.log('Client connected')
-  socket.on('disconnect', () => console.log('Client disconnected'))
-})
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+const buildSocket = require('./server')
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+buildSocket(server)
 
 app.use(passport.initialize())
 app.use(express.urlencoded({ extended: true }))
