654be3666e5589d31e172e67c75c24740d84a655
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 10:29:17 +0800

    嘗試在雲端使用socket.io 6

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ const router = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(passport.initialize())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use('/api', CORSHeader, router)
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
-
-// socket設定
+// socket設定(原本)
+// const server = require('http').createServer(app)
+// const buildSocket = require('./server')
+// buildSocket(server)
+// server.listen(PORT)
 const socketIO = require('socket.io')
-const meaasge = require('./server/modules/message')
-const io = socketIO(app, {
+const INDEX = '/index.html'
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+const io = socketIO(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
@@ -31,12 +30,18 @@ const io = socketIO(app, {
   rejectUnauthorized: false
 })
 io.on('connection', (socket) => {
-  // 可以在伺服器端顯示通道過來的所有事件，以及相關的參數
-  socket.onAny((event, ...args) => {
-    console.log(event, args)
-  })
-  socket.on('disconnect', (reason) => console.log(reason))
-  meaasge(io, socket)
+  console.log('Client connected')
+  socket.on('disconnect', () => console.log('Client disconnected'))
 })
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+
+app.use(passport.initialize())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/api', CORSHeader, router)
+
+app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 module.exports = app
