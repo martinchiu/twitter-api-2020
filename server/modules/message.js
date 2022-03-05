1ab39b7dee4cb315ce844792eed4a913ed10e673
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 11:27:41 +0800

    feat: 根據前後端協定改通道

--- server/modules/message.js
+++ server/modules/message.js
@@ -2,20 +2,27 @@ const { User, Message } = require('../../models')
 const moment = require('moment')
 
 module.exports = (io, socket) => {
-  let userData = []
-
+  let onlineUsers = []
+  console.log(socket)
+  console.log(io)
   socket.on('login', loginUserId => {
     User.findByPk(loginUserId)
       .then(user => {
         if (!user) {
           io.sockets.emit('loginFail', { message: '輸入錯誤使用者Id，無法登入' })
         } else {
-          userData.push(user.dataValues.id)
-          const message = {
+          onlineUsers.push(user.dataValues)
+          io.sockets.emit('message', {
             message: `歡迎${user.dataValues.name}加入聊天室`,
-            userNumber: userData.length
-          }
-          io.sockets.emit('addUser', message)
+            source: 'server',
+            userData: user,
+            createdAt: moment().format(),
+            action: 'join'
+          })
+          io.sockets.emit('userListUpdate', {
+            onlineUsers,
+            onlineUserNumber: onlineUsers.length
+          })
           Message.findAll({
             raw: true,
             nest: true,
@@ -27,25 +34,41 @@ module.exports = (io, socket) => {
                 ...i,
                 createdAt: moment(i.createdAt).fromNow()
               }))
-              return socket.emit('loginSuccess', { loginUserId: user.dataValues.id, userName: user.dataValues.name, messageData })
+              return socket.emit('loginSuccess', {
+                message: '登入成功',
+                loginUserId: user.dataValues.id,
+                userName: user.dataValues.name,
+                messageData,
+                onlineUsers,
+                onlineUserNumber: onlineUsers.length
+              })
             })
         }
       })
   })
 
   /* 監聽登出事件 */
-  socket.on('logout', logoutUserId => {
-    userData = userData.filter(i => i !== logoutUserId)
-    const message = {
-      message: `${logoutUserId}離開聊天室`,
-      userNumber: userData.length
-    }
-    io.sockets.emit('leave', message)
+  socket.on('logout', data => {
+    onlineUsers = onlineUsers.filter(i => i !== data.userId)
+    io.sockets.emit('message', {
+      message: `${data.userName}離開聊天室`,
+      source: 'server',
+      userData: data,
+      createdAt: moment().format(),
+      action: 'leave'
+    })
+    io.sockets.emit('userListUpdate', {
+      onlineUsers,
+      onlineUserNumber: onlineUsers.length
+    })
   })
 
   /* 接收訊息 */
-  socket.on('sendMessage', data => {
-    Message.create(data)
-      .then(() => io.sockets.emit('receiveMessage', data))
+  socket.on('message', data => {
+    Message.create({
+      userId: data.userData.id,
+      message: data.message
+    })
+      .then(message => io.sockets.emit('message', message))
   })
 }
