3abb5c48f4a1ad57521c2c02d6899b24cb4ad64d
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-05 09:14:39 +0800

    嘗試在雲端使用socket.io 2

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ module.exports = (server) => {
   const io = require('socket.io')(server, {
     cors: {
       origin: '*',
+      methods: ['GET', 'POST'],
       allowedHeaders: ['Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'],
       credentials: true
-    }
+    },
+    rejectUnauthorized: false
   })
   io.on('connection', (socket) => {
+    // 可以在伺服器端顯示通道過來的所有事件，以及相關的參數
+    socket.onAny((event, ...args) => {
+      console.log(event, args)
+    })
+    socket.on('disconnect', (reason) => console.log(reason))
     meaasge(io, socket)
   })
 }
