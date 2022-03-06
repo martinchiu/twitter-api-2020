a190ed354c222644249e63f65544f519b9a09575
martinchiu <martin87713@gmail.com>
2022-03-06 09:43:44 +0800

    feat: add private message channel draft

--- server/modules/privateMessage.js
+++ server/modules/privateMessage.js
@@ -1,4 +1,4 @@
-const { User } = require('../../models')
+const { User, PrivateMessage } = require('../../models')
 const createRoomName = require('../../utils/roomName')
 
 module.exports = (io, socket) => {
@@ -6,7 +6,26 @@ module.exports = (io, socket) => {
     const roomName = createRoomName(...data)
     socket.join(roomName)
   })
-  socket.on('privateMessage', data => {
-    
+  socket.on('privateMessage', async data => {
+    // 預設前端回傳的格式：
+    // data {
+    //   sendUserId: 1,
+    //   listenUserId: 2,
+    //    message: '又要變天了...'
+    // }
+    const roomName = createRoomName(...data)
+    const privateMessage = await PrivateMessage.create({
+      sendUserId: data.sendUserId,
+      listenUserId: data.listenUserId,
+      message: data.message
+    })
+    io.to(roomName).emit('message', { privateMessage: privateMessage.dataValues })
+    // 回傳前端的格式：
+    // privateMessage {
+    //   sendUserId: 1,
+    //   listenUserId: 2,
+    //   message: '又要變天了...',
+    //   createdAt: 2022 - 03 - 05 07: 03: 30
+    // }
   })
 }
