b6550007daaacf0cf8cd4dacc42862e1d70c125e
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-03-06 14:58:28 +0800

    更新CORS設定2

--- server/index.js
+++ server/index.js
@@ -5,11 +5,9 @@ module.exports = (server) => {
   const io = require('socket.io')(server, {
     cors: {
       origin: '*',
-      methods: ['GET', 'POST'],
       allowedHeaders: ['Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'],
       credentials: true
-    },
-    rejectUnauthorized: false
+    }
   })
   io.on('connection', (socket) => {
     // 可以在伺服器端顯示通道過來的所有事件，以及相關的參數
